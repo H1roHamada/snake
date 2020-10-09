8f32ba6c2b76a6364be7a9893d99b858b03d3218
Hiro <undefined@example.com>
2020-10-10 04:19:02 +1000

    version 1.0.0

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ function itemPos() { //генератор позиции фрукта
 function randomFruit(arr) { //выбор случайного фрукта
     let random = Math.floor(Math.random() * arr.length);
     fruit = arr[random];
-    fruitFill.src = `img/food/${fruit}.png`;
+    fruitFill.src = `gitimg/food/${fruit}.png`;
     itemPos(); //выбираем случайную позицию
     return fruit;
 }
@@ -130,25 +130,3 @@ function control(event) { //управление
     cooldown = true;
     setTimeout(function () { cooldown = false; }, 100);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
